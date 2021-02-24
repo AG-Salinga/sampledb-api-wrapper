207339b8d40537bb0af85d560fab429c0fca7230
Nils Cedric Holle <nils@hollemail.de>
2021-02-24 19:18:45 +0100

    Add some first example doc

--- sampledbapi/actions.py
+++ sampledbapi/actions.py
@@ -10,12 +10,23 @@ __all__ = ["getList", "get"]
 
 
 def getList() -> List:
-    """Get a list of all actions."""
+    """Get a list of all actions.
+
+    Returns:
+        List: `See here. <https://scientific-it-systems.iffgit.fz-juelich.de/SampleDB/developer_guide/api.html#actions>`_
+    """
     return getData("actions")
 
 
 def get(action_id: int) -> Dict:
-    """Get the specific action (action_id)."""
+    """Get the specific action (action_id).
+
+    Args:
+        action_id (int): ID of the action to be retrieved.
+
+    Returns:
+        Dict: `See here. <https://scientific-it-systems.iffgit.fz-juelich.de/SampleDB/developer_guide/api.html#actions>`_
+    """
     if isinstance(action_id, int):
         return getData("actions/{}".format(action_id))
     else:
--- sampledbapi/actiontypes.py
+++ sampledbapi/actiontypes.py
@@ -6,12 +6,23 @@ __all__ = ["getList", "get"]
 
 
 def getList() -> List:
-    """Get a list of all action types."""
+    """Get a list of all action types.
+
+    Returns:
+        List: `See here. <https://scientific-it-systems.iffgit.fz-juelich.de/SampleDB/developer_guide/api.html#action-types>`_
+    """
     return getData("action_types")
 
 
 def get(type_id: int) -> Dict:
-    """Get the specific action type (type_id)."""
+    """Get the specific action type (type_id).
+
+    Args:
+        type_id (int): ID of the action type.
+
+    Returns:
+        Dict: `See here. <https://scientific-it-systems.iffgit.fz-juelich.de/SampleDB/developer_guide/api.html#action-types>`_
+    """
     if isinstance(type_id, int):
         return getData("actions/{}".format(type_id))
     else:
