88f50a9d744036c154a09750ae29e0a70d10b448
Nils <nils.weber@uni-muenster.de>
2023-03-06 12:51:38 +0100

    Fix tests for utilities

--- sampledbapi/utils.py
+++ sampledbapi/utils.py
@@ -16,7 +16,7 @@ def json2timeseries(data: dict) -> TimeSeries:
     return TimeSeries(data)
     
 def timeseries2json(timeseries: TimeSeries) -> Dict:
-    return {'_type': 'timeseries', 'data': timeseries.data, 'units': timeseries.units} 
+    return {'_type': 'timeseries', 'data': str(timeseries.data), 'units': str(timeseries.units)} 
     
 def json2objectreference(data: dict) -> int:
     if data['_type'] != 'object_reference':
@@ -24,7 +24,7 @@ def json2objectreference(data: dict) -> int:
     return int(data['object_id'])
     
 def objectreference2json(obj_id: int) -> Dict:
-    return {'_type': 'object_reference', 'object_id': obj_id}
+    return {'_type': 'object_reference', 'object_id': str(obj_id)}
 
 def json2object(data: dict) -> objects.Object:
     if data['_type'] != 'object_reference':
@@ -32,7 +32,7 @@ def json2object(data: dict) -> objects.Object:
     return objects.get(int(data['object_id']))
 
 def object2json(obj: objects.Object) -> Dict:
-    return {'_type': 'object_reference', 'object_id': obj.object_id}    
+    return {'_type': 'object_reference', 'object_id': str(obj.object_id)}    
 
 def json2bool(data: dict) -> bool:
     if data['_type'] != 'bool':
@@ -40,7 +40,7 @@ def json2bool(data: dict) -> bool:
     return data['value'] == 'True'
 
 def bool2json(value: bool) -> Dict:
-    return {'_type': 'bool', 'value': value}
+    return {'_type': 'bool', 'value': str(value)}
     
 def json2quantity(data: dict) -> Quantity:
     if data['_type'] != 'quantity':
@@ -48,7 +48,7 @@ def json2quantity(data: dict) -> Quantity:
     return Quantity(data)
 
 def quantity2json(quantity: Quantity) -> Dict:
-    return {'_type': 'quantity', 'value': quantity.value, 'units': quantity.units}
+    return {'_type': 'quantity', 'value': str(quantity.value), 'units': str(quantity.units)}
     
 def json2datetime(data: dict) -> datetime:
     if data['_type'] != 'datetime':
